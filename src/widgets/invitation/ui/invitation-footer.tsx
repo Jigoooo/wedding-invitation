a911893edb922cab6c8db4ed53b7e0ff14c9e5ff
Jigoooo <jiwook97@gmail.com>
2024-11-03 13:06:33 +0900

    참석가능여부 추가

--- src/widgets/invitation/ui/invitation-footer.tsx
+++ src/widgets/invitation/ui/invitation-footer.tsx
@@ -1,3 +1,4 @@
+import { useSearchParams } from 'react-router-dom';
 import { Box, Stack, Typography } from '@mui/joy';
 
 import LinkIcon from '@mui/icons-material/Link';
@@ -11,9 +12,12 @@ import { useShareKakao } from '@/entities/kakao';
 const LINK_URL = import.meta.env.VITE_INVITATION_SERVER_URL;
 
 export function InvitationFooter() {
+  const [searchParams] = useSearchParams();
   const shareKakao = useShareKakao({
     targetUrl: LINK_URL,
   });
+
+  const isCompanyFooterHide = searchParams.get('isCompanyFooterHide') === 'Y';
 
   return (
     <Stack sx={{ width: '100%', alignItems: 'center', gap: 3, py: 2 }}>
@@ -40,20 +44,22 @@ export function InvitationFooter() {
           <Typography sx={{ fontSize: '0.85rem', fontWeight: 500 }}>링크주소 복사하기</Typography>
         </Box>
       </Stack>
-      <Box sx={{ display: 'flex', alignItems: 'center' }}>
-        <Typography
-          sx={{
-            display: 'flex',
-            alignItems: 'center',
-            gap: 0.5,
-            fontSize: '0.7rem',
-            fontWeight: 600,
-            color: '#666666',
-          }}
-        >
-          ⓒ 2024. CopyRight <FuturIcon width={16} height={16} /> Futur All Rights Reserved.
-        </Typography>
-      </Box>
+      {!isCompanyFooterHide && (
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <Typography
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              gap: 0.5,
+              fontSize: '0.7rem',
+              fontWeight: 600,
+              color: '#666666',
+            }}
+          >
+            ⓒ 2024. CopyRight <FuturIcon width={16} height={16} /> Futur All Rights Reserved.
+          </Typography>
+        </Box>
+      )}
     </Stack>
   );
 }
