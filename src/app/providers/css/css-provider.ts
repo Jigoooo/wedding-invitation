4af49a4b8bd63706a92478a182eccf135f305bb3
Jigoooo <jiwook97@gmail.com>
2024-11-03 23:20:20 +0900

    참석가능여부 추가

--- src/app/providers/css/css-provider.ts
+++ src/app/providers/css/css-provider.ts
@@ -1,4 +1,5 @@
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import 'react-medium-image-zoom/dist/styles.css';
 import '@fontsource/inter';
 
 import '@/shared/assets/css/font.css';
